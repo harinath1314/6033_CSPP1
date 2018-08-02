11bd45ae6b1c0cafd84dab4482f5e9f9b75efe9e
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-02 20:12:09 +0530

    " -> 4 of 4 passed. pylint: -3.33/10 "

--- m4/p1/vowels_counter.py
+++ m4/p1/vowels_counter.py
@@ -4,7 +4,7 @@ if you give string as hari then number of vowels = 2
 '''
 
 def main():
-    s = input()
+    s = raw_input()
     number_of_vowels = 0
     # s is the given input
     for letter in s:
