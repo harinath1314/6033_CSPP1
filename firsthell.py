d134684e6f3343dca1f93a5c0322e9490fcaac36
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-02 19:12:21 +0530

    " -> 1 of 1 passed. pylint: 6.00/10 "

--- firsthell.py
+++
@@ -1,7 +0,0 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> print ("hello world")
-hello world
->>> 
-print (hi hello world this is git boy)
