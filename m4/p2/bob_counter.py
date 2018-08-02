9651da0f89993e09bdfc5e4d76927e4b4e389850
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-02 22:47:40 +0530

    " -> 5 of 5 passed. pylint: 7.78/10 "

--- m4/p2/bob_counter.py
+++ m4/p2/bob_counter.py
@@ -1,6 +1,7 @@
 ''' this problem deals with counting of certain word repetition in a given string
-for example if we give a input string as hariiharioharimyhari and ask to find the number of times hari has been repeated,
- and the out should come as 3
+for example if we give a input string as hariiharioharimyhari 
+and ask to find the number of times hari has been repeated,
+and the output should come as 3
 '''
 
 def main():
@@ -12,7 +13,7 @@ def main():
     # the input string is in s
     for i in range(len(string)-2):
          if string[i] == 'b' and string[i+1] == 'o' and string[i+2] == 'b':
-            number_of_bobs  += 1
+            number_of_bobs += 1
     print(number_of_bobs)
 
 if __name__ == "__main__":
