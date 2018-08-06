3d977fb4efb4026fc5d7e64c64df448166f19e0a
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-06 23:18:46 +0530

    " -> 0 of 10 passed. pylint: 9.63/10 "

--- m7/Functions - Assignment-2/assignment2.py
+++ m7/Functions - Assignment-2/assignment2.py
@@ -19,7 +19,7 @@ def paying_debtoffinayear(start_balance, annual_interestrate):
         month_count += 1
         monthly_unpaid_balance = (start_balance) - (min_monthlypayment)
         start_balance = (monthly_unpaid_balance) + (monthly_interest_rate*monthly_unpaid_balance)
-        if start_balance <0:
+        if start_balance < 0:
             ha_ri = 0
         if month_count == 12 and ha_ri != 0:
             min_monthlypayment += 10
@@ -33,7 +33,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment: ", (paying_debtoffinayear(data[0], data[1])))
-    
+    print("Lowest Payment: ",(paying_debtoffinayear(data[0], data[1])))
 if __name__ == "__main__":
     main()
