a3937c1b0ec2a3480473b50e5562d17dc68da673
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-07 00:00:46 +0530

    " -> 0 of 10 passed. pylint: 9.20/10 "

--- m7/Functions - Assignment-3/assignment3.py
+++ m7/Functions - Assignment-3/assignment3.py
@@ -3,7 +3,7 @@ o find the smallest monthly payment to the cent
 (no more multiples of $10) such that we can pay off the debt
 within a year. Try it out with large inputs, and notice
 how fast it is (try the same large inputs in your solution
-to Problem 2 to compare!). 
+to Problem 2 to compare!)
 '''
 def payingdebtoffina_year(balance_in, annual_interestrate):
     '''
@@ -13,7 +13,7 @@ def payingdebtoffina_year(balance_in, annual_interestrate):
     monthly_interest_rate = annual_interestrate / 12
     lower_payment = initial_balance / 12
     upper_payment = (initial_balance * (1 + monthly_interest_rate) ** 12) / 12.0
-    elipson_x= 0.03
+    elipson_x = 0.03
     while abs(balance_in > elipson_x):
         moynthly_payrate = (upper_payment + lower_payment) / 2
         balance_in = initial_balance
@@ -34,7 +34,7 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print(payingdebtoffina_year(data[0],data[1]))
+    print("Lowest Payment:", str(payingdebtoffina_year(data[0],data[1])))
     
 if __name__ == "__main__":
     main()
