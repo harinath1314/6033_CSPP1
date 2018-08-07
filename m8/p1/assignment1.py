2049cb969160f684a5afc31a9ce6f831a5ab47d5
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-07 19:48:50 +0530

    " -> 1 of 10 passed. pylint: 1.11/10 "

--- m8/p1/assignment1.py
+++ m8/p1/assignment1.py
@@ -3,31 +3,25 @@ this is a program which uses recursive technique
 to find the factorial of the given num_ber
 '''
 
-def factorial_of_given_number(n):
+def factorial_of_given_number(num_ber):
     '''
     n is positive integer
 
     returns: a positive integer, the factorial of n.
     '''
-    num_ber = n
-    if n == 0:
+    if num_ber == 0:
         return 1
     else:
-        return n*factorial_of_given_number(n-1)
-    
-    
-
-
+        return num_ber*factorial_of_given_number(n-1)
 def main():
     '''
     this is the main program 
     to calculate
-    the factorial 
+    the factorial
     of the given positive integer
     '''
     give_input = input()
-    print(factorial_of_given_number(int(give_input)))    
-
+    print(factorial_of_given_number(int(give_input)))
 if __name__ == "__main__":
     main()
 
