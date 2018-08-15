45e879dc1100f76e6951871cc90a6bb52e3d8d68
harinath1314 <harinathareddy@harinathas-Air.mshome.net>
2018-08-15 20:37:06 +0530

    " -> 3 of 8 passed. pylint: 6.32/10 "

--- codecamp 1/CodeCampPoker/poker.py
+++ codecamp 1/CodeCampPoker/poker.py
@@ -169,15 +169,15 @@ if __name__ == "__main__":
     # test the poker function to see how it works
     print(' '.join(poker(HANDS)))
 
-COUNT = int(input())
-# iterate through the test cases to set up hands list
-HANDS = []
-for x in range(COUNT):
-    line = input()
-    ha = line.split(" ")
-    HANDS.append(ha)
-# test the poker function to see how it works
-print(' '.join(poker(HANDS)))
+# COUNT = int(input())
+# # iterate through the test cases to set up hands list
+# HANDS = []
+# for x in range(COUNT):
+#     line = input()
+#     ha = line.split(" ")
+#     HANDS.append(ha)
+# # test the poker function to see how it works
+# print(' '.join(poker(HANDS)))
 
 
 
