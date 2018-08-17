9e41852c061cfacf6f8d6e12e2f2e5f0eed85ca8
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-17 19:24:08 +0530

    " -> 1 of 3 passed. pylint: 8.54/10 "

--- codecamp 1/CodeCampDocumentDistance/document_distance.py
+++ codecamp 1/CodeCampDocumentDistance/document_distance.py
@@ -1,21 +1,21 @@
 '''
-    Document Distance - A detailed description is given in the PDF
+    Document Distance - A plagarism calculator
 '''
 import re
 import math
-stop_file='stopwords.txt'
+stop_file = 'stopwords.txt'
 def similarity(input1, input2,cleande_words):
     '''
         Compute the document distance as given in the PDF
     '''
-    temp1=0
-    temp2=0
-    temp3=0
-    dictionary_={}
+    temp1 = 0
+    temp2 = 0
+    temp3 =0  
+    dictionary_ = {}
     for o_o_word in cleande_words:
-        dictionary_[o_o_word]=[int(input1.count(o_o_word)), int(input2.count(o_o_word))]
+        dictionary_[o_o_word] = [int(input1.count(o_o_word)), int(input2.count(o_o_word))]
     for keys in dictionary_:
-        temp1+= dictionary_[keys][0] * dictionary_[keys][1]
+        temp1 += dictionary_[keys][0] * dictionary_[keys][1]
     for keys in dictionary_:
         temp2 += dictionary_[keys][0] * 2
     for keys in dictionary_:
@@ -37,26 +37,23 @@ def main():
     '''
         take two inputs and call the similarity function
     '''
-    input1=input()
+    input1 = input()
     for hari in input1:
         if hari in "!@#$%^&*()_,.1234567890`~/":
-            input1=input1.replace(hari,'')
+            input1 = input1.replace(hari,'')
 
     clean_1 = (input1.lower().strip()).split()
-    
 
-    input2=input()
+    input2 = input()
     for hari in input2:
         if hari in "!@#$%^&*()_,.1234567890`~/":
-            input2=input2.replace(hari,'')
+            input2 = input2.replace(hari, '')
     clean_2 = (input2.lower().strip()).split()
-    
-
 
     stop_words = load_stopwords(stop_file)
-    clean_2=[word for word in clean_2 if word not in stop_words]
-    clean_1=[word_ for word_ in clean_1 if word_ not in stop_words]
-    cleande_words=list(set().union(clean_1,clean_2))
-    print(similarity(clean_1, clean_2,cleande_words))
+    clean_2 = [word for word in clean_2 if word not in stop_words]
+    clean_1 = [word_ for word_ in clean_1 if word_ not in stop_words]
+    cleande_words = list(set().union(clean_1, clean_2))
+    print(similarity(clean_1, clean_2, cleande_words))
 if __name__ == '__main__':
     main()
