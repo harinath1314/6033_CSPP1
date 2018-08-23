6cdd5182b6ab2898ecd2a61c6dcd815fd86fd4cd
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-23 12:19:07 +0530

    " -> 4 of 8 passed. pylint: 8.50/10 "

--- m20/CodeCampMatrixOperations/matrix_operations.py
+++ m20/CodeCampMatrixOperations/matrix_operations.py
@@ -65,7 +65,7 @@ def read_matrix(rows, columns):
         inputt = input().split(' ')
         try:
             for j in range(columns):
-                if len(inputt)==len(mat[j]):
+                if len(inputt)==len(mat[i]):
                     mat[i][j] = int(inputt[j])
         except:
             raise Error("Invalid input for the matrix")
