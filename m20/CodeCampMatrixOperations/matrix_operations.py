d913176ff5cd651d39d76cfcebf91f4a21f17185
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-23 14:51:48 +0530

    " -> 5 of 8 passed. pylint: 8.89/10 "

--- m20/CodeCampMatrixOperations/matrix_operations.py
+++ m20/CodeCampMatrixOperations/matrix_operations.py
@@ -76,7 +76,7 @@ def read_matrix(rows, columns):
         for i in range(rows):
             inputt = input().split(' ')
             for j in range(columns):
-                assert len(inputt) == len(mat)
+                assert len(inputt) == len(mat[0])
                 mat[i][j] = int(inputt[j])
         return mat
     except:
