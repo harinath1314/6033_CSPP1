685fc0d5a11eec15f903c105f113dc87d04b8092
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-23 14:23:09 +0530

    " -> 5 of 8 passed. pylint: 9.29/10 "

--- m20/CodeCampMatrixOperations/matrix_operations.py
+++ m20/CodeCampMatrixOperations/matrix_operations.py
@@ -105,7 +105,7 @@ def main():
         # multiply matrix 1 and matrix 2
         m3_ = mult_matrix(row, column, m1_, m2_)
         print(m3_)
-    except as Exception(e):
+    except Exception as e:
         print(e)
 
 
