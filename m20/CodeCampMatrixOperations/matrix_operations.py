8450bf05e79ece0c1855986983f4bb641a2f2ae7
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-23 11:20:57 +0530

    " -> 0 of 8 passed. pylint: 7.92/10 "

--- m20/CodeCampMatrixOperations/matrix_operations.py
+++ m20/CodeCampMatrixOperations/matrix_operations.py
@@ -57,7 +57,7 @@ def read_matrix(rows, columns):
             mat[i][j] = 0
     for i in range(rows):
         for j in range(columns):
-            inpu = input.split()
+            inpu = input().split()
             mat[i][j] = int(inpu[j])
     return mat
 
