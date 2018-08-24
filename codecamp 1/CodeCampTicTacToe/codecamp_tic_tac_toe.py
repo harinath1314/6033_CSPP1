5522680a6cf2aec92e20c35771ee94ca69aa9a6b
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-24 10:24:43 +0530

    " -> 1 of 14 passed. pylint: 7.57/10 "

--- codecamp 1/CodeCampTicTacToe/codecamp_tic_tac_toe.py
+++ codecamp 1/CodeCampTicTacToe/codecamp_tic_tac_toe.py
@@ -20,6 +20,22 @@ def read_matrix():
         print('invalid input')
         return False
     return mat
+def is_game_valid(tic_tac):
+    temp = 0
+    for i in tic_tac:
+        if len(set(tic_tac[i])) == 1:
+            temp+=1
+    if temp <= 1:
+        return True
+    return False
+
+
+def winner_ti_ta(tic_tac):
+    if is_game_valid:
+        pass
+    else:
+        print('invalid game')
+
 
 import re
 def main():
@@ -27,9 +43,9 @@ def main():
     the main function starts here
     '''
     #read the inputs
-    read_matrix()
-    if read_matrix:
-        pass
+    tic_tac = read_matrix()
+    if tic_tac:
+        winner_ti_ta(tic_tac)
 
 
 if __name__ == '__main__':
