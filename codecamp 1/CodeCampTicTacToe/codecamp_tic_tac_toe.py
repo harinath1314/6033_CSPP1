4cb4a750b05bfe4ec5f56efb14b859eaa560bdb0
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-24 11:46:20 +0530

    " -> 14 of 14 passed. pylint: 5.90/10 "

--- codecamp 1/CodeCampTicTacToe/codecamp_tic_tac_toe.py
+++ codecamp 1/CodeCampTicTacToe/codecamp_tic_tac_toe.py
@@ -1,50 +1,46 @@
 '''
-this is a program for
-tic_tac_toe winner
+tic_tac_toe winner selection
 '''
 def read_matrix():
-    rows = 3
-    columns = 3
+    rows,columns = 3,3
     mat = []
     for i in range(rows):
         inputt = input().split(' ')
-        if inputt.count('x')+inputt.count('o')+inputt.count('.') == 3:
+        if inputt.count('x') + inputt.count('o') + inputt.count('.') == 3:
             mat.append(inputt)
         else:
             print('invalid input')
             return False
     return mat
 def is_game_valid(tic_tac):
-    temp = 0
+    '''
+    shows whether matrix is valid or not
+    '''
+    temp_, x_co, o_co = 0, 0, 0
     for i in range(len(tic_tac)):
+        x_co += tic_tac[i].count('x')
+        o_co += tic_tac[i].count('o')
         if len(set(tic_tac[i])) == 1:
-            temp+=1
-    if temp <= 1:
+            temp_ += 1
+    if temp_ <= 1 and (x_co - o_co) != 3:
         return True
-    else:
-        return False
-
-
+    return False
 def winner_ti_ta(tic_tac):
-    x=0
-    o=0
-    tes=0
+    '''
+    outputs the winner
+    '''
+    x_,o_,tes_ = 0,0,0
     if is_game_valid(tic_tac):
         for i in range(len(tic_tac)):
-            x+=tic_tac[i].count('x')
-            o+=tic_tac[i].count('o')
-            tes+=tic_tac[i].count('.')
-
-        if (x-o==1 or o-x==1) and tes==0:
+            x_ += tic_tac[i].count('x')
+            o_ += tic_tac[i].count('o')
+            tes_ += tic_tac[i].count('.')
+        if (x_ - o_ == 1 or o_ - x_ == 1) and tes_ == 0:
             return 'draw'
-        if x>o:
+        if x_ > o_:
             return 'x'
         return 'o'
-    else:
-        return 'invalid game'
-
-
-
+    return 'invalid game'
 def main():
     '''
     the main function starts here
@@ -52,9 +48,6 @@ def main():
     tic_tac = read_matrix()
     if tic_tac:
         print(winner_ti_ta(tic_tac))
-
-
-
 if __name__ == '__main__':
     main()
 
