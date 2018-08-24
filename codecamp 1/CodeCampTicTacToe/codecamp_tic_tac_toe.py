195829b1df5c91edf6ac246c01fbca4187f866d9
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-24 09:55:02 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

---
+++ codecamp 1/CodeCampTicTacToe/codecamp_tic_tac_toe.py
@@ -0,0 +1,62 @@
+'''
+this is a program for
+tic_tac_toe winner
+'''
+def read_matrix():
+    rows = 3
+    columns = 3
+    mat = []
+    for i in range(rows):
+        mat.append([])
+    for i in range(rows):
+        for j in range(columns):
+            mat[i].append(j)
+            mat[i][j] = '.'
+    print(mat)
+    for i in range(rows):
+        inputt = input().split(' ')
+        if inputt.count('x')+inputt.count('o')+inputt.count('.') == 3:
+            for j in range(columns):
+                mat[i][j] = int(inputt[j])
+        print('invalid input')
+        return False
+    return mat
+
+import re
+def main():
+    '''
+    the main function starts here
+    '''
+    #read the inputs
+    read_matrix()
+    if read_matrix:
+        
+
+    
+        
+
+    print(mat)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
