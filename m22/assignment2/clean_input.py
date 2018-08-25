31325aa3083fa8997080bf91f55c221309dca1da
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-25 10:51:00 +0530

    " -> 3 of 3 passed. pylint: 8.89/10 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -1,5 +1,5 @@
 '''
-Write a function to clean up a given string by removing the special characters and retain 
+Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 re.sub(r'[^a-z\s]', '', one_str)
 '''
@@ -9,7 +9,7 @@ def clean_string(string):
     '''
     cleans the given string
     '''
-    string = re.sub('[^a-z,0-9]','', string)
+    string = re.sub(r'[^a-z,0-9]', '', string)
     return string
 
 def main():
