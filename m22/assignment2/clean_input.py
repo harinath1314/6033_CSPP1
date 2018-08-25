db95eb694e0c8d5f077786b9428809b8799b9777
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-08-25 10:49:34 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -1,12 +1,21 @@
 '''
 Write a function to clean up a given string by removing the special characters and retain 
 alphabets in both upper and lower case and numbers.
+re.sub(r'[^a-z\s]', '', one_str)
 '''
+import re
 
 def clean_string(string):
-    pass
+	'''
+	cleans the given string
+	'''
+    string = re.sub('[^a-z,0-9]','', string)
+    return string
 
 def main():
+	'''
+	this is th amain function
+	'''
     string = input()
     print(clean_string(string))
 
